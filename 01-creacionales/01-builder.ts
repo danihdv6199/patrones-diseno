9f7f07125c5acc6cdea2c14600d515153f6754cd
danihdv6199 <dani6199@gmail.com>
2025-08-14 18:40:32 +0200

    builder

--- 01-creacionales/01-builder.ts
+++ 01-creacionales/01-builder.ts
@@ -12,3 +12,75 @@
  *
  * https://refactoring.guru/es/design-patterns/builder
  */
+
+import { COLORS } from "../helpers/colors.ts";
+
+class Computer {
+  public cpu: string = "cpu - not defined";
+  public ram: string = "ram - not defined";
+  public storage: string = "storage - not defined";
+  public gpu?: string;
+
+  displayConfiguration() {
+    console.log(`Configuracion de la computadorea
+        CPU: ${this.cpu}
+        RAM: ${this.ram}
+        Almacenamietno: ${this.storage}
+        GPU: ${this.gpu ?? "No tiene GPU"}
+        `);
+  }
+}
+
+class ComputerBuilder {
+  private computer: Computer;
+
+  constructor() {
+    this.computer = new Computer();
+  }
+
+  setCPU(cpu: string): ComputerBuilder {
+    this.computer.cpu = cpu;
+    return this;
+  }
+
+  setRAM(ram: string): ComputerBuilder {
+    this.computer.ram = ram;
+    return this;
+  }
+
+  setStorage(storage: string): ComputerBuilder {
+    this.computer.storage = storage;
+    return this;
+  }
+
+  setGPU(gpu: string): ComputerBuilder {
+    this.computer.gpu = gpu;
+    return this;
+  }
+
+  build() {
+    return this.computer;
+  }
+}
+
+function main() {
+  const basicComputer = new ComputerBuilder()
+    .setCPU("Intel Core 2 Duo")
+    .setRAM("4GB")
+    .setStorage("256GB")
+    .build();
+
+  console.log("%Computadora básico:", COLORS.blue);
+  basicComputer.displayConfiguration();
+
+  const gamingComputer = new ComputerBuilder()
+    .setCPU("Intel Core 5")
+    .setRAM("")
+    .setStorage("256GB")
+    .build();
+
+  console.log("%Computadora gaming:", COLORS.cyan);
+  gamingComputer.displayConfiguration();
+}
+
+main();
