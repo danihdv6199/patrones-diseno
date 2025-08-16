5403b5e186d7a3f548b66691b4666889698cc290
danihdv6199 <dani6199@gmail.com>
2025-08-16 11:52:20 +0200

    Factory method

--- 01-creacionales/02-factory-method.ts
+++ 01-creacionales/02-factory-method.ts
@@ -12,3 +12,61 @@
  * https://refactoring.guru/es/design-patterns/factory-method
  *
  */
+
+import { COLORS } from "../helpers/colors.ts";
+
+interface Hamburger {
+  prepare(): void;
+}
+
+class ChickenHamburguer implements Hamburger {
+  prepare(): void {
+    console.log("Preparando hamburguesa de %pollo", COLORS.yellow);
+  }
+}
+
+class BeefHamburguer implements Hamburger {
+  prepare(): void {
+    console.log("Preparando hamburguesa de %cres", COLORS.yellow);
+  }
+}
+
+abstract class Restaurant {
+  abstract createHamburguer(): Hamburger;
+
+  orderHamburger(): void {
+    const hamburger = this.createHamburguer();
+    hamburger.prepare();
+  }
+}
+
+class ChickenRestaurant extends Restaurant {
+  override createHamburguer(): Hamburger {
+    return new ChickenHamburguer();
+  }
+}
+
+class BeefRestaurant extends Restaurant {
+  override createHamburguer(): Hamburger {
+    return new BeefHamburguer();
+  }
+}
+
+function main() {
+  let restaurant: Restaurant;
+  const burgerType = prompt("Que tipo de hamburgesa quieres? (chicken/beef)");
+  switch (burgerType) {
+    case "chicken":
+      restaurant = new ChickenRestaurant();
+      break;
+    case "beef":
+      restaurant = new BeefRestaurant();
+      break;
+    default:
+      throw new Error("Opcion no valida");
+  }
+
+  restaurant.orderHamburger();
+}
+
+main();
